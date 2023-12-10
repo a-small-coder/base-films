63edfeea45de9f954838b8b226e151bb728cd33a
a-small-coder <hulktv222@mail.ru>
2023-12-10 21:40:21 +0300

    :rocket: create project

---
+++ src/components/#FilmList/PreviewBox/index.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import Image from "next/image";
+import * as Style from "./index.styled";
+import img from '../../../../public/img.png'
+
+const PreviewBox = () => {
+    return (
+        <>
+        <Style.Structure>
+            <Style.TextContent>
+                <Style.Heding>
+                    ShowHub
+                </Style.Heding>
+                <Style.Subtitle>
+                    enjoy watching
+                </Style.Subtitle>
+                <Style.Desctiption>
+                        Lorem ipsum dolor sit amet, consectetuer 
+                        adipiscing elit, sed diam nonummy nibh euismod 
+                        tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+                        Ut wisi enimad minim veniam, quis nostrud exerci Lorem ipsum dolor sit amet, 
+                        consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt 
+                        ut laoreet dolore magna aliquam erat volutpat. Ut wisi enimad minim veniam, 
+                        quis nostrud exerci
+                </Style.Desctiption>
+                <a href="/films">
+                <Style.Button>
+                    start now
+                </Style.Button>
+                </a>
+                
+            </Style.TextContent>
+
+            <Style.ImageWrapp>
+            <Image
+                src={img}
+                width={500}
+                height={500}
+                alt="Picture of the author"
+                />
+            </Style.ImageWrapp>
+        </Style.Structure>
+           
+        </>
+    )
+}
+
+export default PreviewBox;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { VscSymbolColor } from "react-icons/vsc";
+import * as Style from "./index.styled";
+import Button from "../Button/Button";
+
+type HeaderProps = {
+  switchTheme: () => void;
+};
+const Header: React.FC<HeaderProps> = (props) => {
+  return (
+    <Style.Header id="header">
+      <Style.Content>
+        <Style.Logo>
+          <Style.Img src="https://yts.mx/assets/images/website/logo-YTS.svg"></Style.Img>
+          <Style.Name>HD movies at the smallest file size.</Style.Name>
+        </Style.Logo>
+
+        <Style.Buttons>
+          <Button
+            name={<VscSymbolColor />}
+            action={() => props.switchTheme()}
+          />
+        </Style.Buttons>
+      </Style.Content>
+    </Style.Header>
+  );
+};
+export default Header;
